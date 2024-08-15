59d653517abba1ae6ea4f93d40b78aac8056e472
johannes karoff <johannes@karoff.net>
2024-08-15 18:42:46 +0200

    add a cli for dbcodegen

---
+++ cli/src/main/scala/dbcodegen/Main.scala
@@ -0,0 +1,47 @@
+package dbcodegen
+
+import mainargs.{arg, main, ParserForMethods}
+
+import java.io.File
+import scala.util.Using
+
+object Main {
+  @main
+  def run(
+    @arg(doc = "The jdbc URL for the database")
+    jdbcUrl: String,
+    @arg(doc = "Output path for the generated code")
+    outDir: String,
+    @arg(doc = "The template file for the code generator")
+    templateFile: Seq[String],
+    @arg(doc = "Scala version to format the code with scalafmt")
+    scalaVersion: Option[String],
+    @arg(doc = "Optional database username")
+    username: Option[String],
+    @arg(doc = "Optional database password")
+    password: Option[String],
+  ) = {
+    val dbConfig = DbConfig(
+      jdbcUrl = jdbcUrl,
+      username = username,
+      password = password,
+    )
+
+    val codeGeneratorConfig = CodeGeneratorConfig(
+      templateFiles = templateFile.map(s => new File(s)),
+      outDir = new File(outDir),
+      typeMapping = (_, tpe) => tpe,
+      schemaTableFilter = (_, _) => true,
+      scalafmt = scalaVersion.isDefined,
+      scalaVersion = scalaVersion.getOrElse("3.0.0"),
+    )
+
+    Using.resource(DbConnection.getSource(dbConfig)) { connectionSource =>
+      val _ = CodeGenerator.generate(connectionSource, codeGeneratorConfig)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val _ = ParserForMethods(this).runOrExit(args.toSeq)
+  }
+}
