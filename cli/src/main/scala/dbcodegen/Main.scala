b2aec31e7da9069302ded48320c7b24c6a80e0c8
johannes karoff <johannes@karoff.net>
2024-08-15 19:10:26 +0200

    add schema-table-filter option to cli

--- cli/src/main/scala/dbcodegen/Main.scala
+++ cli/src/main/scala/dbcodegen/Main.scala
@@ -14,6 +14,10 @@ object Main {
     outDir: String,
     @arg(doc = "The template file for the code generator")
     templateFile: Seq[String],
+    @arg(doc = "Which database schemas to process")
+    schema: Seq[String],
+    @arg(doc = "Which database tables to process")
+    table: Seq[String],
     @arg(doc = "Scala version to format the code with scalafmt")
     scalaVersion: Option[String],
     @arg(doc = "Optional database username")
@@ -31,7 +35,8 @@ object Main {
       templateFiles = templateFile.map(s => new File(s)),
       outDir = new File(outDir),
       typeMapping = (_, tpe) => tpe,
-      schemaTableFilter = (_, _) => true,
+      schemaTableFilter =
+        (schemaName, tableName) => (schema.isEmpty || schema.contains(schemaName)) && (table.isEmpty || table.contains(tableName)),
       scalafmt = scalaVersion.isDefined,
       scalaVersion = scalaVersion.getOrElse("3.0.0"),
     )
